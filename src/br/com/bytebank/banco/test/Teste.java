eb48119cc11f067a4c03043c770cc01c343c9a3f
Charles Willian <wcharlesw15@gmail.com>
2019-06-25 14:02:30 -0300

    Add files via upload

---
+++ src/br/com/bytebank/banco/test/Teste.java
@@ -0,0 +1,8 @@
+package br.com.bytebank.banco.test;
+
+public class Teste {
+    public static void main(String[] args) {
+        int [] idades; // também é possível usar da seguinte forma "int idades []", mas é mais comum do outro jeito
+
+    }
+}
